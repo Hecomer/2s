548b5f819efa0ffbcc67b58fe0a03bb96585f25c
Hecomer <yan.lyandin111@yandex.ru>
2021-04-26 22:47:39 +0500

    сделал

---
+++ 2sem/dz3/3.6.py
@@ -0,0 +1,14 @@
+def success(login):
+    pass
+
+
+def failure(login, message):
+    pass
+
+
+def ask_password(login, password, success, failure):
+    letter_count, login, password, full_copy = 0, login.lower(), password.lower(), False
+    pass
+
+
+print(ask_password('login', 'lygun', success, failure))
